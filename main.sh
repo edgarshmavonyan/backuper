adc0343f2648e927bece2780193e00924ea327e4
edgarshmavonyan <shmavonyan.eha@phystech.edu>
2018-02-18 23:58:14 +0300

    added script

--- main.sh
+++ main.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo 'here starts the new era of my scipts'
+backupFolder=$1
+outputArchive=$2
+
+mkdir $backupFolder &> /dev/null
+
+for fileExtension in "$@"
+do
+	find ~ -name *.$fileExtension -exec cp -prv '{}' $backupFolder/ ';' &> /dev/null
+done
+tar -zcf $outputArchive.tar.gz $backupFolder &> /dev/null
+
+echo "done"
